1afb33030d86bf0bfbe69c7bf10e0db5af223f79
Mingjian-Zhu <zhumingjian@zju.edu.cn>
2021-01-07 18:01:01 +0800

    delete unuseful comment

--- configs/dyfpn/faster_rcnn_r50_dyfpn_1x_coco.py
+++ configs/dyfpn/faster_rcnn_r50_dyfpn_1x_coco.py
@@ -3,7 +3,6 @@ _base_ = '../faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py'
 model = dict(
     neck=dict(
         type='DyFPN_B_CNNGate',
-        # type='Dense_FPN',
         in_channels=[256, 512, 1024, 2048],
         out_channels=256,
         num_outs=5))
